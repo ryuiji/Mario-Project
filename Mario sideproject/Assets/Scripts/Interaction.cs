6b1c9f1e2474f91024c052c73c4a3516df22d315
ryuiji <superrief@hotmail.com>
2015-11-20 09:53:50 +0100

    commit

--- Mario sideproject/Assets/Scripts/Interaction.cs
+++ Mario sideproject/Assets/Scripts/Interaction.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Interaction : MonoBehaviour {
 
+    public RaycastHit upHit;
+    public GameObject shroomPrefab;
+    void Start() {
 
-	void Start () {
-	
-	}
-	
+    }
 
-	void Update () {
-	
-	}
+
+    void Update() {
+        Vector3 up = transform.TransformDirection(Vector3.up);
+        if (Physics.Raycast(transform.position, up, out upHit, GetComponent<MarioStats>().hitDis)) {
+            if (upHit.transform.tag == "itemCube") {
+                Instantiate(shroomPrefab, GameObject.FindGameObjectWithTag("itemCube").transform.position + new Vector3(0,+1.5f,0), transform.rotation);
+            }
+        }
+    }
 }
